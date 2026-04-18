beec28359d3085b5f2fce342075093efe878cb2d
velikohristov <152211378+velikohristov@users.noreply.github.com>
2026-04-18 12:46:01 +0300

    Add files via upload

---
+++ MovieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Изпит
+{
+    public class MovieDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string? PosterUrl { get; set; }
+        public string? TrailerLink { get; set; }
+        public bool? IsWatched { get; set; }
+    }
+}
